acf5015c041d287f20700193761f2b92d340d982
Paula <paulahildebrando2@gmail.com>
2025-08-28 15:28:00 -0300

    Criando a logica do botao salvar e testanto o projeto

--- SistemaPizzaria/Pedido.cs
+++ SistemaPizzaria/Pedido.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -44,44 +45,92 @@ namespace SistemaPizzaria
             else if (cmbTamanhoPizza.SelectedIndex == 1)
             {
                 valorPizza = 30;
-            }else if (cmbTamanhoPizza.SelectedIndex == 2)
+            } else if (cmbTamanhoPizza.SelectedIndex == 2)
             {
                 valorPizza = 50;
             }
             if (chkBorda.Checked == true)
             {
-                valorOpcao = + 5;
+                valorOpcao = +5;
             }
-            if (chkTempero.Checked ==true)
+            if (chkTempero.Checked == true)
             {
-                valorOpcao = + 6;
+                valorOpcao = +6;
             }
             if (chkCebola.Checked == true)
             {
-                valorOpcao = + 3;
+                valorOpcao = +3;
             }
             if (chkCatupiry.Checked == true)
             {
-                valorOpcao = + 4;
+                valorOpcao = +4;
             }
-            else 
+            else
             {
             }
             valorTotal = valorPizza + valorOpcao;
-            txtValorPizza.Text =Convert.ToString(valorPizza);
-            txtValorOpcionais.Text =Convert.ToString(valorOpcao);
+            txtValorPizza.Text = Convert.ToString(valorPizza);
+            txtValorOpcionais.Text = Convert.ToString(valorOpcao);
             txtValorPagar.Text = Convert.ToString(valorTotal);
         }
 
         private void grpOpcionais_Enter(object sender, EventArgs e)
         {
-            cmbTamanhoPizza.SelectedIndex = - 1;
-            txtvalorPizza.Clear();
+            cmbTamanhoPizza.SelectedIndex = -1;
+            txtValorPizza.Clear();
             txtValorOpcionais.Clear();
             chkBorda.Checked = false;
             chkTempero.Checked = false;
             chkCebola.Checked = false;
             chkCatupiry.Checked = false;
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            //VERIFICAR OS CAMPOS
+            if (txtValorPizza.Text == "")
+            {
+                MessageBox.Show("Campo Obrigatório");
+                txtValorOpcionais.Focus();
+            }
+            else if (txtValorOpcionais.Text == "")
+            {
+                MessageBox.Show("Campo Obrigatório");
+                txtValorOpcionais.Focus();
+            }
+            else
+            {
+                //tratamento de erros
+                try
+                {
+                    //inserindo dados no banco
+                    string sql = "insert into tbPedido(tipoPizza,valorPizza,valorOpcao,valorToSqltal) values(@pizza,@vpizza,@vopcao,@total)";
+                    MySqlCommand cmd = new MySqlCommand(sql, con.ConnectarBD());
+                    cmd.Parameters.Add("@pizza", MySqlDbType.Text).Value = cmbTamanhoPizza.Text;
+                    cmd.Parameters.Add("@vpizza", MySqlDbType.Text).Value = txtValorPizza.Text;
+                    cmd.Parameters.Add("@vopcao", MySqlDbType.Text).Value = txtValorOpcionais.Text;
+                    cmd.Parameters.Add("@total", MySqlDbType.Text).Value = txtValorPagar.Text;
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Dados cadastrados com Sucesso !!!");
+                    cmbTamanhoPizza.Text = "";
+                    txtValorPizza.Text = "";
+                    txtValorOpcionais.Text = "";
+                    txtValorPagar.Text = "";
+                    cmbTamanhoPizza.Focus();
+                    con.DesConnectarBD();
+                }
+
+                catch ( Exception erro)
+                {
+                    MessageBox.Show(erro.Message);
+                }
+
+
+
+
+            }
 
         }
     }
